c978c1c9e84794ba438cd8cf41133b8f65f40d2a
evi <herencsareva@gmail.com>
2020-04-13 22:02:06 +0200

    Added all tests

--- tests.py
+++ tests.py
@@ -42,8 +42,46 @@ class Test_Reservation_Template(unittest.TestCase):
         self.assertEqual(self.res_1.identify(5, 'atlas', 'James'), (False, 'for'))
 
 
+class Mock_Reservation(object):
+    _ids = count(0)
+
+    def __init__(self, from_, to, book, for_):
+        self._id = next(self._ids)
+        self._from = from_
+        self._to = to
+        self._book = book
+        self._for = for_
+
+    def overlapping(self, other):
+        return False
+
+    def includes(self, date):
+        return False
+
+    def identify(self, date, book, user):
+        return False
+
+    def change_for(self, new_user):
+        self._for = new_user
+
+
+class Mock_Reservation_True_overlapping(Mock_Reservation):
+    def overlapping(self, other):
+        return True
+
+
+class Mock_Reservation_True_includes(Mock_Reservation_True_overlapping):
+    def includes(self, date):
+        return True
+
+
+class Mock_Reservation_True_identify(Mock_Reservation):
+    def identify(self, date, book, user):
+        return True
+
 class Test_Library_Template(unittest.TestCase):
     def setUp(self):
+        Mock_Reservation._ids = count(0)
         self.lib = Library_Template()
         self.lib._users.add('Kevin')
         self.lib._users.add('Peter')
@@ -63,66 +101,33 @@ class Test_Library_Template(unittest.TestCase):
         self.assertEqual(self.lib.add_book('d'), 1)
 
     def test_reserve_book(self):
-        class Mock_Reservation(object):
-            _ids = count(0)
-
-            def __init__(self, from_, to, book, for_):
-                self._id = next(self._ids)
-                self._from = from_
-                self._to = to
-                self._book = book
-                self._for = for_
-
-            def overlapping(self, other):
-                return (self._book == other._book and self._to >= other._from
-                        and self._from <= other._to)
-
-            def includes(self, date):
-                return (self._from <= date <= self._to)
-
         self.assertEqual(self.lib.reserve_book('Eva', 'a', 10, 15, Mock_Reservation), (False, 'user'))
         self.assertEqual(self.lib.reserve_book('Kevin', 'a', 15, 10, Mock_Reservation), (False, 'date'))
         self.assertEqual(self.lib.reserve_book('Kevin', 'd', 10, 20, Mock_Reservation), (False, 'book'))
         self.assertEqual(self.lib.reserve_book('Peter', 'b', 15, 30, Mock_Reservation), (True, 0))
-        self.assertEqual(self.lib.reserve_book('Kevin', 'b', 10, 20, Mock_Reservation), (False, 'reservation'))
-        self.assertEqual(self.lib.reserve_book('Kevin', 'a', 10, 30, Mock_Reservation), (True, 2))
+        Mock_Reservation._ids = count(0)
+        self.assertEqual(self.lib.reserve_book('Kevin', 'c', 50, 80, Mock_Reservation_True_includes), (True, 0))
+        self.assertEqual(self.lib.reserve_book('Peter', 'c', 50, 80, Mock_Reservation_True_includes), (False, 'reservation'))
+        Mock_Reservation._ids = count(0)
+        self.assertEqual(self.lib.reserve_book('Kevin', 'a', 10, 30, Mock_Reservation), (True, 0))
 
-    def test_check_reservation(self):
-        class Mock_Reservation(object):
-            def __init__(self, date, book, user):
-                self.date = date
-                self.user = user
-                self.book = book
+    def test_check_reservation_exist(self):
+        self.lib._reservations.append(Mock_Reservation_True_identify(5, 10, 'a', 'Kevin'))
+        self.assertTrue(self.lib.check_reservation('Kevin', 'a', 6))
 
-            def identify(self, date, book, user):
-                if book == 'a':
-                    return True
-                return False
-
-        self.lib._reservations.append(Mock_Reservation(10, 'a', 'Kevin'))
-        self.assertTrue(self.lib.check_reservation('Kevin', 'a', 5))
-        self.assertFalse(self.lib.check_reservation('Kevin', '5', 5))
+    def test_check_reservation_not_exist(self):
+        self.lib._reservations.append(Mock_Reservation(5, 10, 'a', 'Peter'))
+        self.assertFalse(self.lib.check_reservation('Peter', 'a', 5))
 
     def test_change_reservation(self):
-        class Mock_Reservation(object):
-            def __init__(self, date, book, user):
-                self.date = date
-                self.user = user
-                self.book = book
-
-            def identify(self, date, book, user):
-                if book == 'a':
-                    return True
-                return False
-            def change_for(self, new_user):
-                self.user = new_user
-
-        self.lib._reservations.append(Mock_Reservation(10, 'a', 'Kevin'))
-        self.lib.add_user('Jane')
+        self.lib._reservations.append(Mock_Reservation_True_identify(5,10, 'a', 'Kevin'))
+        self.lib._users.add('Jane')
         self.assertEqual(self.lib.change_reservation('Kevin', 'a', 10, 'Jane'), (True, 'true'))
         self.assertEqual(self.lib.change_reservation('Kevin', 'a', 10, 'NoUser'), (False, 'new_user'))
-        self.assertEqual(self.lib.change_reservation('Kevin', 'b', 10, 'NoUser'), (False, 'reservation'))
 
+    def test_change_reservation_no_relevant_reservation(self):
+        self.lib._reservations.append(Mock_Reservation(5,10, 'a', 'Kevin'))
+        self.assertEqual(self.lib.change_reservation('Kevin', 'b', 10, 'NoUser'), (False, 'reservation'))
 
 class MockPrinter(object):
     def print(self, string):
